2120ecd63c27d2a2557bf50925f755ad44f6706c
cghimire <chiranghimire44@gmail.com>
2019-03-08 22:59:29 -0500

    Delete config.php

--- Website Files/connection/config.php
+++
@@ -1,7 +0,0 @@
-<?php
-    define('DB_HOST', 'pluto');
-    define('DB_USER', 'lg13');
-    define('DB_PASSWORD', 'DBstudent16');
-    define('DB_DATABASE', 'lg13');
-    define('APP_NAME', 'Restaurant Management');
-?>
